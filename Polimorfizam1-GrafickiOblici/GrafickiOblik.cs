37b17397e65d807898d2b2fde0a7b92d05559e96
Mihael <mihael.kukec@mev.hr>
2019-01-10 08:59:54 +0100

    Razredi, osnovne metode, dijagram nasljeđivanja. metoda Main dorađena.

---
+++ Polimorfizam1-GrafickiOblici/GrafickiOblik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfizam1_GrafickiOblici
+{
+    class GrafickiOblik
+    {
+        public int Boja { get; set; } = 0;
+        public int SirinaCrte { get; set; } = 1;
+
+        public void nacrtaj()
+        {
+            Console.WriteLine("Razred GrafickiOblik"
+                + "- Metoda: nacrtaj()");
+        }
+
+    }
+}
---
+++ Polimorfizam1-GrafickiOblici/Oblici/Trokut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizam1_GrafickiOblici
+{
+    class Trokut : GrafickiOblik
+    {
+        public void nacrtaj()
+        {
+            Console.WriteLine("Razred " + GetType() + " - Metoda: nacrtaj()");
+        }
+    }
+}
